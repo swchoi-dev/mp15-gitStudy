b74dd68d09eac07fc168f8d52cfee21789c18143
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-27 11:05:42 +0900

    [Feat] Player 클래스 추가

---
+++ ConsoleApp2/ConsoleApp2/Player.cs
@@ -0,0 +1,17 @@
+namespace ConsoleApp2;
+
+public class Player
+{
+    private string _name;
+    private int _level;
+
+    public string Name => _name;
+
+    public int Level => _level;
+    
+    public Player(string name, int level)
+    {
+        _name = name;
+        _level = level;
+    }
+}
